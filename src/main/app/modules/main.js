78f51b8e6335f07a47b14c8a963828afdb97d62c
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-10-18 18:04:38 +0300

    Move routes declaration to routes.js

--- src/main/app/modules/main.js
+++ src/main/app/modules/main.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-import App from './components/App';
-import About from './components/About';
+import { Router, hashHistory } from 'react-router';
+import { getRoutes } from './routes';
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-    <div>
-        <Router history={hashHistory}>
-            <Route path='/' component={App}/>
-            <Route path='/about' component={About}/>
-        </Router>
-    </div>,
+    <Router history={hashHistory} routes={getRoutes()}/>,
     app
 );
---
+++ src/main/app/modules/routes.js
@@ -0,0 +1,1 @@
+import React from 'react';import { Route } from 'react-router';import App from './components/App';import About from './components/About';export const getRoutes = () => {    return (        <Route>            <Route path='/' component={App}/>            <Route path='/about' component={About}/>        </Route>    );};
