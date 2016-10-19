7973fc2b3f784814ff7b8ed45627ec8dd7a91a6f
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-10-19 16:40:10 +0300

    Use HashHistory

--- src/main/app/modules/main.js
+++ src/main/app/modules/main.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
-import {browserHistory} from 'react-router';
+import {hashHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 const app = document.getElementById('app');
 
 ReactDOM.render(
