06d9ff84f62fc51c48408b0581f417b8405db19d
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-10-19 16:25:03 +0300

    Connect application with react-router-redux

--- src/main/app/modules/main.js
+++ src/main/app/modules/main.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
-import {hashHistory} from 'react-router';
+import {browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 const app = document.getElementById('app');
 
 ReactDOM.render(
-    <Root store={store} history={hashHistory}/>,
+    <Root store={store} history={history}/>,
     app
 );
--- src/main/app/modules/reducers/index.js
+++ src/main/app/modules/reducers/index.js
@@ -1,1 +1,1 @@
-const rootReducer = (state, action) => {    return state;};export default rootReducer;
+import {combineReducers} from 'redux';import {routerReducer as routing} from 'react-router-redux';const appReducer = combineReducers({    routing});const rootReducer = (state, action) => {    return appReducer(state, action);};export default rootReducer;
