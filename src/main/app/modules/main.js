3aab985603f195fb4ae8da3c6ba44fad3a3176a2
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-10-20 19:30:33 +0300

    Change HashHistory to BrowserHistory

--- src/main/app/modules/main.js
+++ src/main/app/modules/main.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
-import {hashHistory} from 'react-router';
+import {browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
-const history = syncHistoryWithStore(hashHistory, store);
+const history = syncHistoryWithStore(browserHistory, store);
 const app = document.getElementById('app');
 
 ReactDOM.render(
